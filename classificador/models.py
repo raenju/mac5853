9b3acd39358fa9df071df3d06cdc19c0720f4ee8
Rodrigo Enju <rodrigo.enju@usp.br>
2018-10-04 12:34:37 -0300

    Modelos do banco de dados

--- classificador/models.py
+++ classificador/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Dominio(models.Model):
+	url = models.CharField(max_length=100)
+	legal = models.BooleanField()
+
+class Pagina(models.Model):
+	url = models.CharField(max_length=200)
+	domain = models.ForeignKey(Dominio, on_delete=models.CASCADE)
+	legal = models.BooleanField()
+
+class Motivo(models.Model):
+	nome = models.CharField(max_length=100)
+
+class DominioRestritoPor(models.Model):
+	id_d = models.ForeignKey(Dominio, on_delete=models.CASCADE)
+	nome_m = models.ForeignKey(Motivo, on_delete=models.CASCADE)
+
+class PaginaRestritoPor(models.Model):
+	id_p = models.ForeignKey(Pagina, on_delete=models.CASCADE)
+	nome_m = models.ForeignKey(Motivo, on_delete=models.CASCADE)

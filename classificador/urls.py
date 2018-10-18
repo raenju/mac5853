03b852e1a496cbea754c1c084cb05231aaa95e8a
Rodrigo Enju <rodrigo.enju@usp.br>
2018-10-18 11:14:33 -0300

    adicionado bootstrap, index em andamento

--- classificador/urls.py
+++ classificador/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import url
 from django.urls import path
+from django.http import HttpResponseRedirect
 
 from . import views
 
 urlpatterns = [
-    #path('index/', views.index, name='index')
-    path('', views.index, name='index'),
+    path('', lambda r: HttpResponseRedirect('index/')),
+    path('index/', views.index, name='index'),
     path('busca/', views.search, name='busca'),
     path('resultado/', views.answer, name='resposta'),
-    path('lista/', views.querylist, name='lista')
+    path('lista/', views.querylist, name='lista'),
+
+    path('urls_submit/', views.urls_submit, name='urls_submit')
 ]
--- classificador/views.py
+++ classificador/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-
 from . import templates
 
+
+# Chamadas de renderização das páginas
+
 def index(request):
-    context = {}
-    return render(request, 'classificador/index.html', context)
+	vv = [1,2,3,9,8,7]
+	context = {'vec':vv}
+	return render(request, 'classificador/index.html', context)
 
 def search(request):
 	context = {}
@@ -17,3 +20,12 @@ def answer(request):
 def querylist(request):
 	context = {}
 	return render(request, 'classificador/lista.html', context)
+
+
+# Chamadas de formulários
+
+def urls_submit(request):
+	query = request.POST['url_list']
+	vv = [1,2,3,9,8,7, query]
+	context = {'vec':vv}
+	return render(request, 'classificador/index.html', context)

e66d957e4d7c79a1a20ca4960209fb45b676db8b
Rodrigo Enju <rodrigo.enju@usp.br>
2018-10-27 16:44:15 -0300

    testes para os utils

--- classificador/tests/test_utils.py
+++ classificador/tests/test_utils.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
-from classificador.utils.parse_url import get_domain
+from classificador.utils.parse_url import get_domain, parse
+from classificador.utils.html_handler import get_html
 
 # Create your tests here.
 
@@ -16,3 +17,19 @@ class getDomainTestCase(TestCase):
         self.assertEqual(get_domain(google3), 'google.com')
         self.assertEqual(get_domain(google4), 'google.com')
         self.assertEqual(get_domain(google5), 'google.com')
+
+class parseTestCase(TestCase):
+
+    def test_parse(self):
+        str1 = "ab cd ef gh /i -j"
+        str2 = "a1         e2             c3"
+        self.assertEqual(parse(str1), ["ab","cd","ef","gh","/i","-j"])
+        self.assertEqual(parse(str2), ["a1","e2","c3"])
+
+class getHtmlTestCase(TestCase):
+
+    def test_get_html(self):
+        str1 = "abc"
+        str2 = "https://www.google.com/"
+        self.assertEqual(get_html(str1).html, None)
+        self.assertEqual(get_html(str2).error, None)

65a00411348bba14d4674ac85a06bad4fddc04e2
Rodrigo Enju <rodrigo.enju@usp.br>
2018-10-28 15:08:11 -0300

    comecando um classificador simples

--- classificador/utils/classificadores/regex_classif.py
+++ classificador/utils/classificadores/regex_classif.py
@@ -1,4 +1,22 @@
+import re
 
+def busca_armamentos(site):
+	s = site.html
+	matches = 0
+	if re.search('(?i)arma', s) is not None:
+		matches = matches + 1
+	if re.search('(?i)calibre', s) is not None:
+		matches = matches + 1
+	if re.search('(?i)muni[cç][aã]o|(?i)muni[cç][oõ]es', s) is not None:
+		matches = matches + 1
+
+	if matches >= 2:
+		return True
+	return False
 
 def classificate(site):
-	return ["reason1","reason2"]
+	reasons = []
+	if busca_armamentos(site):
+		reasons.append("Armamentos")
+
+	return reasons
